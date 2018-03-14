fcd6e31fdacf273ca6077257668b4afb6c531e0a
tanestila <millafko@gmail.com>
2018-03-14 22:20:02 +0700

    check plugin

--- aspnet/aspnet/Controllers/HomeController.cs
+++ aspnet/aspnet/Controllers/HomeController.cs
@@ -17,49 +17,49 @@ namespace aspnet.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
 
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Upload(HttpPostedFileBase upload)
-        {
-            if (upload != null)
-            {
-                // получаем имя файла
-                string fileName = System.IO.Path.GetFileName(upload.FileName);
-                // сохраняем файл в папку Files в проекте
-                upload.SaveAs(Server.MapPath("~/Files/" + fileName));
-            }
-            return RedirectToAction("Index");
-        }
-        public void Sign()
-        {
-            XmlNode xmlNode = xmlElement.GetElementsByTagName("SignedInfo")[0];
-            XmlDocument xmlDocumentSignInfo = new XmlDocument();
-            xmlDocumentSignInfo.PreserveWhitespace = true;
-            xmlDocumentSignInfo.LoadXml(xmlNode.OuterXml);
-            result = Canonicalize(xmlDocumentSignInfo);
-        }
-        public string Canonicalize(XmlDocument document)
-        {
-            XmlDsigExcC14NTransform xmlTransform = new XmlDsigExcC14NTransform();
-            xmlTransform.LoadInput(document);
-            string result = new StreamReader((MemoryStream)xmlTransform.GetOutput()).ReadToEnd();
-            //C# метод канокализации не добавляет в XPath неймсппейс
-            result = s.Replace("<XPath>", "<XPath xmlns:dsig=\"http://www.w3.org/2000/09/xmldsig#\">");
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Upload(HttpPostedFileBase upload)
+        //{
+        //    if (upload != null)
+        //    {
+        //        // получаем имя файла
+        //        string fileName = System.IO.Path.GetFileName(upload.FileName);
+        //        // сохраняем файл в папку Files в проекте
+        //        upload.SaveAs(Server.MapPath("~/Files/" + fileName));
+        //    }
+        //    return RedirectToAction("Index");
+        //}
+        //public void Sign()
+        //{
+        //    XmlNode xmlNode = xmlElement.GetElementsByTagName("SignedInfo")[0];
+        //    XmlDocument xmlDocumentSignInfo = new XmlDocument();
+        //    xmlDocumentSignInfo.PreserveWhitespace = true;
+        //    xmlDocumentSignInfo.LoadXml(xmlNode.OuterXml);
+        //    result = Canonicalize(xmlDocumentSignInfo);
+        //}
+        //public string Canonicalize(XmlDocument document)
+        //{
+        //    XmlDsigExcC14NTransform xmlTransform = new XmlDsigExcC14NTransform();
+        //    xmlTransform.LoadInput(document);
+        //    string result = new StreamReader((MemoryStream)xmlTransform.GetOutput()).ReadToEnd();
+        //    //C# метод канокализации не добавляет в XPath неймсппейс
+        //    result = s.Replace("<XPath>", "<XPath xmlns:dsig=\"http://www.w3.org/2000/09/xmldsig#\">");
 
 
-            return result;
-        }
+        //    return result;
+        //}
     }
 }
